7a591de10c9578370f764c7feb631df19ed1529a
Sojeong <k5k5s5s5j5j5@naver.com>
2022-02-04 14:22:19 +0900

    모델, 데이터베이스 수정

--- backend/areum/models.py
+++ backend/areum/models.py
@@ -1,5 +1,7 @@
+from unicodedata import category
 from django.db import models
 from datetime import datetime
+
 
 class Edu(models.Model):
     title = models.CharField(max_length=100)
@@ -7,6 +9,7 @@ class Edu(models.Model):
     image = models.TextField(null=True)
     date = models.DateTimeField(default=datetime.now())
     video = models.TextField(null=True)
+    category = models.IntegerField(default=4) #음악 1, 요리 2, 운동 3, 생활 4
 
     def __str__(self):
         """A string representation of the model."""
