8851d7fcba78d35e20cac4f2b01755b80829f4fe
Sojeong <k5k5s5s5j5j5@naver.com>
2022-02-04 14:32:42 +0900

    모델, 데이터베이스 수정

--- backend/areum/models.py
+++ backend/areum/models.py
@@ -7,7 +7,7 @@ class Edu(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(null=True)
     image = models.TextField(null=True)
-    date = models.DateTimeField(default=datetime.now())
+    date = models.DateTimeField(auto_now_add=True)
     video = models.TextField(null=True)
     category = models.IntegerField(default=4) #음악 1, 요리 2, 운동 3, 생활 4
 
